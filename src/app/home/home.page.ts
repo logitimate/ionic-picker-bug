6b8f89fc870b7c5d60ed542185f3068d021fe868
Eric Horodyski <horodyski@ionic.io>
2020-06-17 13:43:04 -0400

    Update to Ionic Angular @ 5.2.2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,51 +1,54 @@
-import {Component} from '@angular/core';
-import {PickerController} from '@ionic/angular';
-import {PickerColumnOption} from '@ionic/core/dist/types/components/picker/picker-interface';
+import { Component } from "@angular/core";
+import { PickerController } from "@ionic/angular";
+import { PickerColumnOption } from "@ionic/core/dist/types/components/picker/picker-interface";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.page.html',
-    styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-    colors: Array<PickerColumnOption> = [
-        {text: 'Black', value: 'Black'},
-        {text: 'Blue', value: 'Blue'},
-        {text: 'Green', value: 'Green'},
-        {text: 'Maroon', value: 'Maroon'},
-        {text: 'Red', value: 'Red'},
-        {text: 'Silver', value: 'Silver'},
-        {text: 'Tan', value: 'Tan'},
-        {text: 'Yellow', value: 'Yellow'},
-        {text: 'White', value: 'White'}
-    ];
+  colors: Array<PickerColumnOption> = [
+    { text: "Black", value: "Black" },
+    { text: "Blue", value: "Blue" },
+    { text: "Green", value: "Green" },
+    { text: "Maroon", value: "Maroon" },
+    { text: "Red", value: "Red" },
+    { text: "Silver", value: "Silver" },
+    { text: "Tan", value: "Tan" },
+    { text: "Yellow", value: "Yellow" },
+    { text: "White", value: "White" },
+  ];
 
-    selectedColor: string;
+  selectedColor: string;
 
-    constructor(private pickerController: PickerController) {
-    }
+  constructor(private pickerController: PickerController) {}
 
-    async openPicker() {
-        const picker = await this.pickerController.create({
-            columns: [{
-                name: 'Color',
-                options: this.colors,
-                selectedIndex: this.colors.findIndex(opt => opt.value === this.selectedColor)
-            }],
-            buttons: [
-                {
-                    text: 'Cancel',
-                    role: 'cancel'
-                },
-                {
-                    text: 'Confirm',
-                    handler: (value) => {
-                        this.selectedColor = value.Color.value;
-                    }
-                }
-            ]
-        });
-        await picker.present();
-    }
-
+  async openPicker() {
+    const picker = await this.pickerController.create({
+      columns: [
+        {
+          name: "Color",
+          options: this.colors,
+          selectedIndex: this.colors.findIndex(
+            (opt) => opt.value === this.selectedColor
+          ),
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "Confirm",
+          handler: (value) => {
+            console.log(value);
+            this.selectedColor = value.Color.value;
+          },
+        },
+      ],
+    });
+    await picker.present();
+  }
 }
